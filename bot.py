6e663754ecf32a165153652b91eaccbe1b1d85fb
KevinSky263 <k0968730673@gmail.com>
2021-05-30 21:17:05 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,4 +7,4 @@ bot = commands.Bot(command_prefix='[')
 async def on_ready():
     print(">> Bot is online <<")
 
-bot.run('ODQ4NTM0NDU2NTU3ODk1NzQw.YLOBSw.SAJuF3UCgl2RILnlIuaNRLmnncM')
+bot.run('ODQ4NTM0NDU2NTU3ODk1NzQw.YLOBSw.oJLpTeNuW2lPsJa7C533RbcI-lw')
