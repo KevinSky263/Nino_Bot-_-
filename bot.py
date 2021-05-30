c72265fd737b6797fe58ae0cb379bb1ba0a8714c
KevinSky263 <k0968730673@gmail.com>
2021-05-30 22:24:59 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,4 +7,19 @@ bot = commands.Bot(command_prefix='[')
 async def on_ready():
     print(">> Bot is online <<")
 
-bot.run('ODQ4NTM0NDU2NTU3ODk1NzQw.YLOBSw.oJLpTeNuW2lPsJa7C533RbcI-lw')
+@bot.event
+async def on_member_join(member):
+    channel = bot.get_channel(848558256439951450)
+    await channel.send(f'{member} join!')
+
+@bot.event
+async def on_member_remove(member):
+    channel = bot.get_channel(848558300760375296)
+    await channel.send(f'{member} leave!')
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send(bot.larency)
+
+
+bot.run('ODQ4NTM0NDU2NTU3ODk1NzQw.YLOBSw.tF4QZg7IaJp-0-fHpgJ-mDfY2y0')
